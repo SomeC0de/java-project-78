27e03e8484e32ab8621504d0b9ae0d1e5d18e2b9
SomeC0de <ingen.ole@yandex.ru>
2024-12-19 13:29:26 +0400

    Add jacoco and unit tests instructions to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
+    checkstyle
+    application
+    jacoco
     id("java")
+    id("com.adarshr.test-logger") version "4.0.0"
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -10,10 +18,21 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
 }
