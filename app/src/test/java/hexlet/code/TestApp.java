fd5325477afae01c654ba39b5905d31c79ca7d49
SomeC0de <ingen.ole@yandex.ru>
2024-12-24 02:59:32 +0400

    Add classes and its unit tests dummies

---
+++ app/src/test/java/hexlet/code/TestApp.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class TestApp {
+    @Test
+    public void dummyTest() {
+        assertEquals(true, true);
+    }
+}
---
+++ app/src/test/java/hexlet/code/TestValidator.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class TestValidator {
+    @Test
+    public void dummyTest() {
+        assertEquals(true, true);
+    }
+}
