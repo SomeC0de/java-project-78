e32126c066f5524f2989e61d0e947bcf9f47cb3c
SomeC0de <ingen.ole@yandex.ru>
2025-02-05 02:42:44 +0400

    Implement basic logic

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,4 +1,35 @@
 package hexlet.code;
 
 public class StringSchema {
+    private int minLength;
+    private String sampleToCheck;
+    private boolean isRequired = false;
+
+    public StringSchema() {
+        this.minLength = 0;
+        this.sampleToCheck = null;
+    }
+
+    public boolean isValid(String input) {
+        if (this.isRequired) {
+            return (input.length() >= this.minLength) && input.contains(this.sampleToCheck);
+        } else {
+            return true;
+        }
+    }
+
+    public StringSchema required() {
+        this.isRequired = true;
+        return this;
+    }
+
+    public StringSchema minLength(int length) {
+        this.minLength = length;
+        return this;
+    }
+
+    public StringSchema contains(String sample) {
+        this.sampleToCheck = sample;
+        return this;
+    }
 }
