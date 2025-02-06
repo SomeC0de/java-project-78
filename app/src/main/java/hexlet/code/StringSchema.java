6bfb302dd6ce0ea8aaa2c3dca1609b23d9437615
SomeC0de <ingen.ole@yandex.ru>
2025-02-07 03:42:30 +0400

    Implement StringSchema passed tests

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,50 +1,55 @@
 package hexlet.code;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 // TBD: required(), minLength() and contains() are all TESTS functions, isValid() is trigger to each function and
 //      we need a storage, which accumulates every TEST that will be performed. After tests are accumulated they all
 //      executes inside stream in isValid().
 public class StringSchema {
-    private int minLength;
-    private String sampleToCheck;
     private boolean isRequired = false;
+    List<Predicate<String>> tests = new ArrayList<>();
 
     public StringSchema() {
-        this.minLength = 0;
-        this.sampleToCheck = null;
         this.isRequired = false;
     }
 
     public boolean isValid(String input) {
-        if (!this.isRequired) {
-            return isInvalidDefault(input);
+        if (!this.isRequired && isInvalidDefault(input)) {
+            return true;
         }
 
-        return isValidRequired(input);
+        for (var verification : tests) {
+            if (!verification.test(input)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public StringSchema required() {
         this.isRequired = true;
+
+        Predicate<String> requiredScheme = str-> str != null && !str.isEmpty();
+        tests.add(requiredScheme);
         return this;
     }
 
     public StringSchema minLength(int length) {
-        this.minLength = length;
+        Predicate<String> lengthScheme = str-> str.length() >= length;
+        tests.add(lengthScheme);
         return this;
     }
 
     public StringSchema contains(String sample) {
-        this.sampleToCheck = sample;
+        Predicate<String> containsScheme = str-> str.contains(sample);
+        tests.add(containsScheme);
         return this;
     }
 
-    private boolean isInvalidDefault(Object obj) {
-        return !(obj instanceof String) || ((String)obj).isEmpty();
-    }
-
-    private boolean isValidRequired(Object obj) {
-        return (obj instanceof String)
-                && !((String)obj).isEmpty()
-                && (((String)obj).length() >= this.minLength)
-                && (((String)obj).contains(this.sampleToCheck));
+    private boolean isInvalidDefault(String s) {
+        return s == null || s.isEmpty();
     }
 }
