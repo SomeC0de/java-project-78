46b248edc064e57d9ba8139aaeaa00ff0d0323ce
SomeC0de <ingen.ole@yandex.ru>
2025-02-20 02:50:16 +0400

    Remove redundant sections from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,10 +6,6 @@ plugins {
     id("com.adarshr.test-logger") version "4.0.0"
 }
 
-application {
-    mainClass = "hexlet.code.App"
-}
-
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
@@ -21,10 +17,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
     testImplementation("org.assertj:assertj-core:3.23.1")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 tasks.test {
